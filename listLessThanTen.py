c36f86f5897428d3273b252f86df7a4f21eb543d
Daniel A. Hardcastle <TheThing99@gmail.com>
2017-06-14 13:09:40 +0200

    added listLessThanTen.py completed and edited functionality

---
+++ listLessThanTen.py
@@ -0,0 +1,11 @@
+value = int(input("Enter number: "))
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+lowlist = []
+
+if value >= 1:
+    for x in a:
+        if x <= value:
+            lowlist.append(x)
+    print(lowlist)
+else:
+    print("number too low")
