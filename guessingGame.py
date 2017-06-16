db4b4c5d4267d3f6362cf593c92a600e55e68eb0
Daniel A. Hardcastle <TheThing99@gmail.com>
2017-06-16 14:45:57 +0200

    edited guessingGame

--- guessingGame.py
+++ guessingGame.py
@@ -1,5 +1,6 @@
 from random import randint
 score = [0, 0]
+choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 while True:
     computer_input = str(randint(1, 9))
     user_input = str(input("Enter guess between 1-9: "))
@@ -10,7 +11,7 @@ while True:
         print("Total games: %d" % (score[0] + score[1]))
         break
 
-    if user_input == "1" or "2" or "3" or "4" or "5":
+    if user_input in choices:
         if user_input == computer_input:
             print("You win!")
             score[0] += 1
