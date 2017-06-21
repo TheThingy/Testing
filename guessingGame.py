5cf2c1541017f164c0b27755e3c115faddd6b251
Daniel A. Hardcastle <TheThing99@gmail.com>
2017-06-21 15:03:56 +0200

    made the choices list better

--- guessingGame.py
+++ guessingGame.py
@@ -1,12 +1,12 @@
 from random import randint
 score = [0, 0]
-choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+choices = [str(x) for x in range(1, 10)]
 while True:
     computer_input = str(randint(1, 9))
     user_input = str(input("Enter guess between 1-9: "))
+    print(choices)
 
     if user_input == "exit":
-        print(user_input)
         print("You have won %d times and lost %d times" % (score[0], score[1]))
         print("Total games: %d" % (score[0] + score[1]))
         break
@@ -17,7 +17,7 @@ while True:
             score[0] += 1
         else:
             print("You loose")
-            print("The computer chose: %s" % (computer_input))
+            print("The computer chose: %s" % computer_input)
             score[1] += 1
     else:
         print("Please enter a number or 'exit'")
